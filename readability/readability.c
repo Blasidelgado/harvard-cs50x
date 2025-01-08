1b4398c210f74d348e20f08dbf90c346d7f08ffd
Blasidelgado <blasidelgado@gmail.com>
2025-01-08 15:07:08 -0300

    week 2: add readability solution

---
+++ readability/readability.c
@@ -0,0 +1,129 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+/** New version of readability
+ * - Now prompts the user until any text is given
+ * - If text consists of spaces only, program exits with code 1
+ * - Program now ignores multiple spaces in a row
+*/
+
+int main(void)
+{
+    // Prompt user to input some text
+    string user_text;
+
+    do
+    {
+        user_text = get_string("Text: ");
+    }
+    while (strlen(user_text) < 1);
+
+    // Calculate total words
+    int W = count_words(user_text);
+
+    // If no word is found, exit program with code 1
+    if (W < 1)
+    {
+        printf("No text found. Exiting\n");
+        return 1;
+    }
+
+    // Calculate average letters per 100 words
+    float L = (float) count_letters(user_text) * 100 / W;
+
+    // Calculate average sentences per 100 words
+    float S = (float) count_sentences(user_text) * 100 / W;
+
+    printf("Words: %i Letters: %.2f Sentences: %f\n", W, L, S);
+
+    // Calculate Coleman-Liau index
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    // Output grade depending on index value
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+
+    return 0;
+}
+
+// Function to count total letters
+int count_letters(string text)
+{
+    int len = strlen(text);
+    int letters = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+
+    return letters;
+}
+
+// Function to count total words
+int count_words(string text)
+{
+    int len = strlen(text);
+
+    // If single character was inputted, check if its alphabetical or not
+    if (len == 1)
+    {
+        return !!isalpha(text[0]);
+    }
+
+    int words = 0;
+
+    // Iterate the string looking for valid spaces
+    for (int i = 1; i < len; i++)
+    {
+        if (isspace(text[i]) && (isalpha(text[i - 1]) || ispunct(text[i - 1])))
+        {
+            words++;
+        }
+    }
+    // Check if an extra word is added at the end of the text
+    if (isalpha(text[len - 1]) || ispunct(text[len - 1]) || text[len - 1] == '?')
+    {
+        words++;
+    }
+
+    return words;
+}
+
+// Function to count total sentences
+int count_sentences(string text)
+{
+    int len = strlen(text);
+    int sentences = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        // Check if char is '.', '!' or '?'
+        if (text[i] == 46 || text[i] == 33 || text[i] == 63)
+        {
+            sentences++;
+        }
+    }
+
+    return sentences;
+}
